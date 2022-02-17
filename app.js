3cab6fe591803272e812717e8078fc78814340f2
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-17 09:32:58 +0600

    error handle for clculation section

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
+// function for taking value from input field
+
 function inputFieldValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputFieldText = inputField.value;
     const inputFieldAmount = parseFloat(inputFieldText)
-    inputField.value = ''
+    inputField.value = ""
     return inputFieldAmount;
 }
 
 
 
+// Income and expenses calculation part
 
 document.getElementById("calculate").addEventListener("click", function () {
     const income = inputFieldValue("income")
     const foodExpenses = inputFieldValue("food-expenses");
     const rentExpenses = inputFieldValue("rent-expenses");
     const clothExpenses = inputFieldValue("cloth-expenses");
-
+    const expenses = document.getElementById("total-expenses");
+    const balace = document.getElementById("balance");
     const totalExpenses = foodExpenses + rentExpenses + clothExpenses;
 
-    const expenses = document.getElementById("total-expenses");
+    if (foodExpenses <= 0 || clothExpenses <= 0 || rentExpenses <= 0) {
+        if (foodExpenses <= 0) {
+            alert("food Expenses cant be nagative")
+        }
+        else if (clothExpenses <= 0) {
+            alert("Cloth expenses cant be nagative")
+        }
+        else {
+            alert("This is not your Fathers hotel. you stay here without zero cost")
+        }
+    }
+    else if (isNaN(income) || isNaN(foodExpenses) || isNaN(rentExpenses) || isNaN(clothExpenses)) {
+        if (isNaN(income)) {
+            alert("enter a valid number in income field")
+        }
+        else if (isNaN(foodExpenses)) {
+            alert("enter a valid number in food expenses field")
+        }
+        else if (isNaN(rentExpenses)) {
+            alert("enter a valid number in rent expenses field")
+        }
+        else {
+            alert("enter a valid number in cloth expenses field")
+        }
+    }
 
-    expenses.innerText = totalExpenses;
+    else if (income < totalExpenses) {
+        alert("income can't be less then tatal Expenses")
+    }
 
-    const balace = document.getElementById("balance");
-    balace.innerText = income - totalExpenses;
-
-
-
+    else {
+        expenses.innerText = totalExpenses;
+        balace.innerText = income - totalExpenses;
+    }
 })
+
